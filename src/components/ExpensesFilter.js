23d38d37f010c9899f767dca4f450b5fda43b34f
Skycoder10 <akashmandalam49973@gmail.com>
2021-11-17 13:40:04 +0530

    number of filtered year increase

--- src/components/ExpensesFilter.js
+++ src/components/ExpensesFilter.js
@@ -12,10 +12,11 @@ const ExpensesFilter = (props) => {
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
         <select value={props.selected} onChange={dropdownChangeHandler}>
-          <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
           <option value='2019'>2019</option>
+          <option value='2019'>2018</option>
+          <option value='2019'>2017</option>
         </select>
       </div>
     </div>
