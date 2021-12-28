5287d9000f784d4a4250d9c6fe656efe6094be97
Skycoder10 <akashmandalam49973@gmail.com>
2021-12-28 19:55:19 +0530

    change in button

--- src/components/ExpensesFilter.js
+++ src/components/ExpensesFilter.js
@@ -15,8 +15,8 @@ const ExpensesFilter = (props) => {
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
           <option value='2019'>2019</option>
-          <option value='2019'>2018</option>
-          <option value='2019'>2017</option>
+          <option value='2018'>2018</option>
+          <option value='2017'>2017</option>
         </select>
       </div>
     </div>
--- src/components/ExpensesItem.js
+++ src/components/ExpensesItem.js
@@ -15,7 +15,7 @@ const ExpensesItem = (props) => {
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
-            <button type='button' className="button-1">Delete</button>
+            
             
         </Card>
     );
